c495ce39de6f1aa6dae1514c9c1914da255b1914
Nebojša Cvetković <nebkat@gmail.com>
2020-09-05 20:10:53 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import '../polyfills';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { HomeModule } from './home/home.module';
+import { DetailModule } from './detail/detail.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    DetailModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
